54c64683116350bac25d7c937355c03610123f22
srajineni <98992293+srajineni@users.noreply.github.com>
2022-05-10 19:36:36 -0700

    Update github-actions-script.sh

--- .github/workflows/github-actions-script.sh
+++ .github/workflows/github-actions-script.sh
@@ -2,7 +2,5 @@
 #! /bin/bash
   echo 2. is Git installed?
   git --version
-   wet -0 - https://github.com:srajineni/java-app/srajineni/main
-   tar -xzf Package.zip
-   cd Package
-   sh install.sh
+   wet -0 https://github.com:srajineni/java-app/srajineni/main
+ 
