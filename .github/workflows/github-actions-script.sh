d4b38983881497e1aa9f6f9e40c89f3a67d6bfb7
srajineni <98992293+srajineni@users.noreply.github.com>
2022-05-10 20:08:28 -0700

    Update github-actions-script.sh

--- .github/workflows/github-actions-script.sh
+++ .github/workflows/github-actions-script.sh
@@ -2,5 +2,5 @@
 #! /bin/bash
   echo 2. is Git installed?
   git --version
-   wet -0  https://github.com/srajineni/java-app/
+  wet -0 pom.xml https://github.com/srajineni/java-app/blob/main/pom.xml
  
