19bd91a1e0c7610e5df20e21b402f7239f0cb57c
srajineni <98992293+srajineni@users.noreply.github.com>
2022-05-12 08:21:21 -0700

    Update github-actions-script.sh

--- .github/workflows/github-actions-script.sh
+++ .github/workflows/github-actions-script.sh
@@ -1,16 +1,5 @@
 
 #! /bin/bash
-  echo 1. is Git installed?
-  git --version
-  echo 2. is wet installed
-  wget -v
-  sudo apt-get install wget
-  wget -h
-  mkdir -p java-wet/Downloads
-  cd java-wet
-  wget https://github.com/srajineni/java-app/blob/
-  if [[ $1 =~ \.zip$ ]]; then
-        wget $1 && sudo unzip *.zip
-  ls
-  fi
- 
+curl \
+  -H "Accept: application/vnd.github.v3+json" \
+https://github.com/srajineni/java-app/suites/6482118976/artifacts/240256515
