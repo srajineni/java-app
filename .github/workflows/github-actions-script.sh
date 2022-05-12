e2b8de5d9cf54038594ea99d0b3c70f27781dd64
srajineni <98992293+srajineni@users.noreply.github.com>
2022-05-12 08:44:38 -0700

    Update github-actions-script.sh

--- .github/workflows/github-actions-script.sh
+++ .github/workflows/github-actions-script.sh
@@ -2,4 +2,4 @@
 #! /bin/bash
 curl \
   -H "Accept: application/vnd.github.v3+json" \
-https://github.com/srajineni/java-app/suites/6482118976/artifacts/240256515
+https://github.com/srajineni/java-app/suites/6482376786/artifacts/240273914
