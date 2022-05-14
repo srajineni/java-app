8998d4747cae5330e4f00f96a93c8da4c6ef613b
srajineni <98992293+srajineni@users.noreply.github.com>
2022-05-14 11:09:46 -0700

    Update github-actions-script.sh

--- .github/workflows/github-actions-script.sh
+++ .github/workflows/github-actions-script.sh
@@ -2,5 +2,5 @@
 #! /bin/bash
 
 curl -u 
-wget rajineni:https://github.com/srajineni/java-app/suites/6507655145/artifacts/241936620
+wget :https://github.com/srajineni/java-app/suites/6507655145/artifacts/241936620
  echo : "connection sucessful"
