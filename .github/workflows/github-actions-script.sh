e47ebda0840d1b385a1f839e9689a2896f76bc65
srajineni <98992293+srajineni@users.noreply.github.com>
2022-05-14 10:17:03 -0700

    Update github-actions-script.sh

--- .github/workflows/github-actions-script.sh
+++ .github/workflows/github-actions-script.sh
@@ -1,20 +1,5 @@
 
 #! /bin/bash
-
-HOST: $127.0.0.1
-PORT: S1085
-USERNAME = $rajineni
-PASSWORD= 
-usernam : $ssh hsiam-dev
-	password:
-	$ ls
-  
-	$ cd SATA_HSIAM
-  $ cd war 	
- $ DIR = "java11"
-	
-  if [ -d "$DIR" ] ; then
-  #!path 
-  $ cp /home/runner/work/java-app/java-app/target/*.war java11
-  
-  fi
+curl -u 
+ $rajineni: $Ranga@997799
+ $echo : "connection sucessful"
