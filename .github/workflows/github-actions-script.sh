8293e73cf07cbc43721a4112896f569e63ce74d1
srajineni <98992293+srajineni@users.noreply.github.com>
2022-05-14 09:42:09 -0700

    Update github-actions-script.sh

--- .github/workflows/github-actions-script.sh
+++ .github/workflows/github-actions-script.sh
@@ -1,5 +1,20 @@
 
 #! /bin/bash
-curl \
-  -H "Accept: application/vnd.github.v3+json" \
-https://github.com/srajineni/java-app/suites/6482376786/artifacts/240273914
+
+HOST: $127.0.0.1
+PORT: S1085
+USERNAME = $rajineni
+PASSWORD= 
+usernam : $ssh hsiam-dev
+	password:
+	$ ls
+  
+	$ cd SATA_HSIAM
+  $ cd war 	
+ $ DIR = "java11"
+	
+  if [ -d "$DIR" ] ; then
+  #!path 
+  $ cp /home/runner/work/java-app/java-app/target/*.war java11
+  
+  fi
